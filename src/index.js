e8227e53b3b64fec18e72931911c53a608b3ab11
Himanshu <himanshukalal1997@gmail.com>
2021-08-27 14:34:20 +0530

    Completed Login or Register with Home page

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ import Controller from './screens/Controller';
 import Header from './common/header/Header';
 
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
